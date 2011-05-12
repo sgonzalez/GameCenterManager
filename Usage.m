c30a390e5c6403554745585748d569049e133d14
Santiago Gonzalez <santiagogonzalez@me.com>
2011-05-12 11:54:52 -0600

    Fixed a few things

--- Usage.m
+++ Usage.m
@@ -10,24 +10,8 @@
 //Put when you post a score to a leaderboard
 [[GameCenterManager sharedGameCenterManager] reportScore:31415926 forCategory:@"leaderboardID"];
 
+//Opening achievement vc
+[[GameCenterManager sharedGameCenterManager] showAchievementsFromViewController:self];
 
-/*Multiplayer*/
-
-//Open matchmaker view controller
-[[GameCenterManager sharedGameCenterManager] findMatchWithMinPlayers:2 maxPlayers:2 fromViewController:self delegate:self]; //assumed that self is a delegate and is a view controller
-
-//Sending data
-NSError *error;
-BOOL success = [[GameCenterManager sharedGameCenterManager].match sendDataToAllPlayers:data withDataMode:GKMatchSendDataReliable error:&error];
-if (!success) {
-	NSLog(@"Error sending init packet");
-	[self matchEnded];
-}
-
-//To recieve invites, in delegate
-- (void)inviteReceived {
-	//Reset game or whatever and get ready
-}
-
-
-You can figure out the rest! ;-)
+//Opening leaderboard vc
+[[GameCenterManager sharedGameCenterManager] showLeaderboardsFromViewController:self];
